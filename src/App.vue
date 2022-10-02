5db6f73af11de4afc5ba9f0f69bac64a8e528584
kom50 <omprakashkartik82280@gmail.com>
2022-10-02 19:24:26 +0530

    Update code

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,8 @@ import HomeView from './components/HomeView.vue'
 </script>
 
 <template>
-  <div class="pt-1 h-screen w-screen p-0 m-0">
+  <div class="pt-1 h-screen w-screen p-0 m-0 ">
     <HomeView />
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+ 
